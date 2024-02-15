7ffdfc54ec69f61015a4d53a4adafae2a1a8a2ce
isagucci <gucci004@newschool.edu>
2024-02-14 22:06:56 -0500

    upd

---
+++ entries/entry1/script.js
@@ -0,0 +1,16 @@
+
+function checkPassword() {
+
+  var enteredPassword = document.getElementById("passwordInput").value;
+
+  var correctPassword = "The Minotaur";
+
+  if (enteredPassword === correctPassword) {
+
+    window.location.href = "../entry2/index.html";
+  
+  } else {
+    // Display an error message if the password is incorrect
+    alert("Incorrect password! Hint: To find the answer you have to look at the SOURCE");
+  }
+}
