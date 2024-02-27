6a545fce593cd7b4cb5496a9b6f30b7b3850645d
isagucci <gucci004@newschool.edu>
2024-02-26 22:55:41 -0500

    u

--- entries/entry6/script.js
+++ entries/entry6/script.js
@@ -3,7 +3,7 @@
     var scrollPosition = window.scrollY;
 
     paragraphs.forEach(function(paragraph, index) {
-      var scaleFactor = index < 7 ? (index + 1) * 0.3 : (15 - index) * 2; 
+      var scaleFactor = index < 7 ? (index + 1) * 0.2 : (15 - index) * 2; 
       var fontSize = 20 + scaleFactor; 
       fontSize = Math.max(15, fontSize); 
       paragraph.style.fontSize = fontSize + 'px';
